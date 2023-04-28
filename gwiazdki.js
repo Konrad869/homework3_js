63da38849d4ff7b49d98587bf0d0e1d5797e1c2b
Konrad Studziński <konrad966wp.pl@wp.pl>
2023-04-28 14:45:42 +0200

    Modify index.html

--- gwiazdki.js
+++ gwiazdki.js
@@ -1,9 +1,15 @@
 "use strict";
 
-for (let i = 1; i <= 5; i++) {
-  let row = "";
-  for (let j = 1; j <= i; j++) {
-    row += "*";
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("gwiazdka");
+
+  for (let i = 1; i <= 5; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += "*";
+    }
+    const rowElement = document.createElement("p");
+    rowElement.innerHTML = row;
+    container.appendChild(rowElement);
   }
-  document.write(row + "<br>");
-}
+});
--- gwiazdki2.js
+++ gwiazdki2.js
@@ -1,8 +1,15 @@
 "use strict";
-for (let i = 5; i >= 1; i--) {
-  let row = "";
-  for (let j = 1; j <= i; j++) {
-    row += "*";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("gwiazdka-dwa");
+
+  for (let i = 5; i >= 1; i--) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += "*";
+    }
+    const rowElement = document.createElement("p");
+    rowElement.innerHTML = row;
+    container.appendChild(rowElement);
   }
-  document.write(row + "<br>");
-}
+});
